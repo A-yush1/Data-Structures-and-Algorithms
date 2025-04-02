ace1b547b9b0d635f16189c5edc30020cb19c590
A-yush1 <ayush123kumar34@gmail.com>
2025-04-03 03:39:34 +0530

    2 April

---
+++ Leetcode/April Daily Challenge/2873. Maximum Value of an Ordered Triplet I.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long maximumTripletValue(int[] nums) {
+        
+        long res = 0;
+        int n = nums.length;
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i+1; j<n; j++)
+            {
+                for(int k=j+1; k<n; k++)
+                {
+                    long val = 1L * (nums[i] - nums[j]) * nums[k];
+                    res = Math.max(res, val);
+                }
+            }
+        }
+
+        return res;
+    }
+}
